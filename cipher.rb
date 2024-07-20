5b60d131a61ec1976753bb590f6f5a10598e2e4e
weirdnomad <petitpasm@icloud.com>
2024-07-20 21:15:27 +0200

    better translations accept symbols and negative value

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,35 @@
 
+
 def cipher(key)
   puts "What is the message to code Sir?"
   sentence = gets.chomp
   string_to_number = sentence.bytes
   
   new_number = []
-  string_to_number.each {|letter| new_number.push(letter + key)}
+  string_to_number.each do |letter|
+    new_letter = letter
+      case letter
+
+        when 65..90
+          if letter + key > 90
+            new_letter = letter + key - 25
+          elsif letter + key < 65
+            new_letter = letter + key + 25      
+          else
+            new_letter = letter + key
+          end
+          
+        when 97..122
+          if letter + key > 122
+              new_letter = letter + key - 25
+          elsif letter + key < 97
+              new_letter = letter + key + 25      
+          else
+              new_letter = letter + key
+          end
+      end
+    new_number.push(new_letter)
+  end 
   resolving_space = new_number.map { |num| num == (32+key) ? 32 : num }
   resolving_space
   
@@ -15,12 +39,14 @@ def cipher(key)
   puts coded_sentence.join
 end
 
-cipher(6)
+def get_key
+  puts "What is the key number Sir? (between -25 and 25)"
+  key = gets.chomp.to_i
+  if key > -25 or key < 25
+    return key
+  else puts "That is not possible!"
+  end
+end
 
 
-
-
-
-
-
-
+cipher(get_key)
